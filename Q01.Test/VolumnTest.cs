bd43bfc7d35ce0c50955e7fb56ed8d7cd1861107
smallfishit <henrychang0202@gmail.com>
2022-04-26 22:14:28 +0800

    Q01 完成第1題

---
+++ Q01.Test/VolumnTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q01.Test
+{
+    /// <summary>體積測試</summary>
+    [TestFixture]
+    public class VolumnTest
+    {
+        /// <summary>
+        /// 體積的測試
+        /// </summary>
+        /// <param name="length">長</param>
+        /// <param name="width">寬</param>
+        /// <param name="height">高</param>
+        /// <param name="volumn">體積</param>
+        [TestCase(8, 10, 12, 960)]
+        [TestCase(7, 9, 20, 1260)]
+        [TestCase(5, 12, 13, 780)]
+        [TestCase(14, 6, 22, 1848)]
+        [Test]
+        public void ShowScoreResult_InputScore_ReturnOutOfRange(int length, int width, int height, int volumn)
+        {
+            //建立物件
+            VolumnCalculator volumnCal = new VolumnCalculator();
+            //顯示結果
+            int result = volumnCal.CalculateVolumn(length, width, height);
+            //驗證結果是否正確
+            Assert.AreEqual(volumn, result);
+        }
+    }
+}
---
+++ Q01/VolumnCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q01
+{
+    /// <summary>體積計算物件</summary>
+    public class VolumnCalculator
+    {
+        /// <summary>
+        /// 計算體積
+        /// </summary>
+        /// <param name="length">長</param>
+        /// <param name="width">寬</param>
+        /// <param name="height">高</param>
+        /// <returns></returns>
+        public int CalculateVolumn(int length, int width, int height)
+        {
+            return length * width * height;
+        }
+    }
+}
